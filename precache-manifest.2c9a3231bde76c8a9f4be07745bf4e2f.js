917feb278793df21cc81fe333b037136a00e3b39
JaiRaga <raga.j.santhosh@gmail.com>
2020-04-05 22:28:34 +0530

    Updates

---
+++ precache-manifest.2c9a3231bde76c8a9f4be07745bf4e2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d07db6132ec5780c7456f190f1474488",
+    "url": "/top-games/index.html"
+  },
+  {
+    "revision": "abd0f60af56bfa1c6ef6",
+    "url": "/top-games/static/css/main.7ac8668f.chunk.css"
+  },
+  {
+    "revision": "42db9a9930e5c1695144",
+    "url": "/top-games/static/js/2.0c4d75d6.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/top-games/static/js/2.0c4d75d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abd0f60af56bfa1c6ef6",
+    "url": "/top-games/static/js/main.bde0f590.chunk.js"
+  },
+  {
+    "revision": "fa346800e7fd8fc3c601",
+    "url": "/top-games/static/js/runtime-main.16fc4c08.js"
+  }
+]);
